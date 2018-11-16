8a2de634a7e013d889cc625f582211d53d47a1ba
caishaodong <1146631758@qq.com>
2018-11-16 11:02:57 +0800

    页面信息配置

--- src/main/java/com/dong/springbootswagger/config/SwaggerConfig.java
+++ src/main/java/com/dong/springbootswagger/config/SwaggerConfig.java
@@ -3,7 +3,10 @@ package com.dong.springbootswagger.config;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,7 +22,21 @@ public class SwaggerConfig {
 
     @Bean
     public Docket swaggerSpringMvcPlugin() {
-        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build();
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API")
+                .description("接口文档API")
+                .termsOfServiceUrl("https://github.com/caishaodong/springboot-swagger")
+                .contact(new Contact("caishaodong", "https://blog.csdn.net/qq_39101581", "1146631758@qq.com"))
+                .version("1.0")
+                .build();
     }
 
 
--- src/test/java/com/dong/springbootswagger/SpringbootSwaggerApplicationTests.java
+++ src/test/java/com/dong/springbootswagger/SpringbootSwaggerApplicationTests.java
@@ -1,16 +1,45 @@
 package com.dong.springbootswagger;
 
+import com.dong.springbootswagger.common.ResObject;
+import com.dong.springbootswagger.controller.UserController;
+import com.dong.springbootswagger.model.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringbootSwaggerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void createUser() {
+        User user = new User("张三", "123456", "杭州");
+        ResponseEntity<ResObject> entity = this.testRestTemplate.postForEntity("/user/create/user", user, ResObject.class);
+        System.out.println("==========> createUser: " + entity.getBody());
+    }
+
+    @Test
+    public void updateUser() {
+        User user = new User("李四", "123456", "上海");
+        ResponseEntity<ResObject> entity = this.testRestTemplate.postForEntity("/user/update/user", user, ResObject.class);
+        System.out.println("==========> updateUser: " + entity.getBody());
+        System.out.println("==========> statusCode: " + entity.getStatusCode());
+        System.out.println("==========> statusCodeValue: " + entity.getStatusCodeValue());
+    }
+
+    @Test
+    public void deleteUser() {
+        this.testRestTemplate.delete("/user/delete/user");
+    }
 
 }
